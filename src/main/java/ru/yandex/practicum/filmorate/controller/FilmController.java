299fceabcbc3c46be3992b7ec4fa59fa180f2eb1
Islandec235 <nrgjay@yandex.ru>
2024-05-01 22:44:08 +0300

    style: скобки перекинуты на одну строку с полями методов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -29,24 +29,21 @@ public class FilmController {
 
     @GetMapping("/popular")
     public List<Film> getTopFilmsByLikes(
-            @RequestParam(value = "count", defaultValue = "10", required = false) int count
-    ) {
+            @RequestParam(value = "count", defaultValue = "10", required = false) int count) {
         return service.getTopFilmsByLikes(count);
     }
 
     @PutMapping("/{filmId}/like/{userId}")
     public void addLikeToFilm(
             @PathVariable(value = "filmId") int filmId,
-            @PathVariable(value = "userId") int userId
-    ) {
+            @PathVariable(value = "userId") int userId) {
         service.addLikeToFilm(filmId, userId);
     }
 
     @DeleteMapping("/{filmId}/like/{userId}")
     public void deleteLikeFromFilm(
             @PathVariable(value = "filmId") int filmId,
-            @PathVariable(value = "userId") int userId
-    ) {
+            @PathVariable(value = "userId") int userId) {
         service.deleteLikeFromFilm(filmId, userId);
     }
 
@@ -61,9 +58,7 @@ public class FilmController {
     }
 
     @DeleteMapping(value = {"", "/{filmId}"})
-    public Film deleteFilmById(
-            @PathVariable(value = "filmId", required = false) Optional<Integer> filmId
-    ) {
+    public Film deleteFilmById(@PathVariable(value = "filmId", required = false) Optional<Integer> filmId) {
         if (filmId.isEmpty()) {
             throw new IllegalArgumentException("При удалении фильма не был передан id");
         }
@@ -74,11 +69,10 @@ public class FilmController {
     @GetMapping("/director/{directorId}")
     public List<Film> getDirectorFilm(
             @PathVariable(value = "directorId") int directorId,
-            @RequestParam(value = "sortBy") String sortBy
-    ) {
+            @RequestParam(value = "sortBy") String sortBy) {
         return service.getDirectorFilm(directorId, sortBy);
     }
-  
+
     @GetMapping("/search")
     public List<Film> searchFilms(@RequestParam String query, @RequestParam String by) {
         return service.searchFilms(query, by);
